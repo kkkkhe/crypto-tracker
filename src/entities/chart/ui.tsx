619ed59dd12152db34bd2e585e358386686d0afe
Dennisss <denzeldenisq@gmail.com>
2022-12-30 22:37:42 +0300

    tailwindcss

--- src/entities/chart/ui.tsx
+++ src/entities/chart/ui.tsx
@@ -43,27 +43,31 @@ export const CoinChartInfo = () => {
 		Tooltip,
 		Legend
 	  );
-	return <Line data={{
-		labels: data?.prices.map((coin:any) => {
-			let date = new Date(coin[0]);
-			let time = date.getHours() > 12
-				? `${date.getHours() - 12}:${date.getMinutes()} PM`
-				: `${date.getHours()}:${date.getMinutes()} AM`;
-			return days === 1 ? time : date.toLocaleDateString();
-		}),
-		datasets: [
-		  {
-			data: data?.prices.map((coin:any) => coin[1]),
-			label: `Price ( Past ${days} Days ) in USD`,
-			borderColor: "#EEBC1D",
-		  },
-		],
-	  }}
-	  options={{
-		elements: {
-		  point: {
-			radius: 1,
-		  },
-		},
-	  }}/>;
+	return <div className='border-l-[1px] border-white'>
+		<div className='pl-[20px] py-[15px]'>
+			<Line data={{
+			labels: data?.prices.map((coin:any) => {
+				let date = new Date(coin[0]);
+				let time = date.getHours() > 12
+					? `${date.getHours() - 12}:${date.getMinutes()} PM`
+					: `${date.getHours()}:${date.getMinutes()} AM`;
+				return days === 1 ? time : date.toLocaleDateString();
+			}),
+			datasets: [
+			{
+				data: data?.prices.map((coin:any) => coin[1]),
+				label: `Price ( Past ${days} Days ) in USD`,
+				borderColor: "#EEBC1D",
+			},
+			],
+		}}
+		options={{
+			elements: {
+			point: {
+				radius: 1,
+			},
+			},
+		}}/>
+		</div>
+		</div>
 }
